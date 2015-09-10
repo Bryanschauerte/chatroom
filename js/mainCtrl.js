b1d7fafe30a63ed1c16454efeac4238a34021ed8
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-09-09 20:07:29 -0600

    sortable, timestamp added and a new background

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,9 +1,30 @@
 var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
+
+$scope.getParseData = function() {
+
+  parseService.getData().then(function(response){
+    console.log(response);
+    $scope.messages = response.data.results;
+// console.log($scope.messages);
+  });
+}
+
+$scope.filteredText = '';
+
+  $scope.postData = function(){
+
+    parseService.postData($scope.message).then(function(){
+      $scope.message = '';
+    });
+
+  }
+
+
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
+  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
 
 
@@ -16,7 +37,7 @@ app.controller('mainCtrl', function($scope, parseService){
 
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
+  setInterval(function(){
+    $scope.getParseData();
+  }, 1500)
 })
